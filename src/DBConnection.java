c2804cbf69ce5dd3ef82270ce027f0d9e49612a6
Brundha M <brundhamohan11@gmail.com>
2026-08-04 01:35:40 +0530

    Update DBConnection.java

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -4,20 +4,18 @@ import java.sql.DriverManager;
 public class DBConnection{
 
     static final String URL= "jdbc:mysql://localhost:3306/course_enrollment";
-    static final String USER= "root";
-    static final String PASSWORD= "root"; 
+    static final String USER= "your_username";
+    static final String PASSWORD= "your_password"; 
 
-    public static Connection getConnection() {
-
-        try {
+    public static Connection getConnection(){
+        try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             return DriverManager.getConnection(URL, USER, PASSWORD);
 
-        } catch (Exception e) {
+        } catch(Exception e){
             System.out.println("Database Connection Failed!");
             e.printStackTrace();
         }
-
         return null;
     }
 }
